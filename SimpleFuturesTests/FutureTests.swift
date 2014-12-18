dcc9049703805f8b77aeee9adbd88b4c3d67a3a9
Troy Stribling <troy.stribling@gmail.com>
2014-12-17 22:03:30 -0800

    added future map tests

--- SimpleFuturesTests/FutureTests.swift
+++ SimpleFuturesTests/FutureTests.swift
@@ -465,11 +465,55 @@ class FutureMapTests : XCTestCase {
     }
 
     func testFailedMapping() {
-        
+        let future = Future<Bool>()
+        let expectationMapped = expectationWithDescription("OnFailure fulfilled for mapped future")
+        let expectation = expectationWithDescription("OnSuccess fulfilled")
+        future.onSuccess {value in
+            XCTAssert(value, "future onSuccess value invalid")
+            expectation.fulfill()
+        }
+        future.onFailure {error in
+            XCTAssert(false, "future onFailure called")
+        }
+        let mapped = future.map {value -> Try<Int> in
+            return Try<Int>(TestFailure.error)
+        }
+        mapped.onSuccess {value in
+            XCTAssert(false, "mapped onSuccess called")
+        }
+        mapped.onFailure {error in
+            expectationMapped.fulfill()
+        }
+        future.success(true)
+        waitForExpectationsWithTimeout(2) {error in
+            XCTAssertNil(error, "\(error)")
+        }
     }
 
     func testMappingToFailedFuture() {
-        
+        let future = Future<Bool>()
+        let expectationMapped = expectationWithDescription("OnFailure fulfilled for mapped future")
+        let expectation = expectationWithDescription("OnFailure fulfilled")
+        future.onSuccess {value in
+            XCTAssert(false, "future onSucces called")
+        }
+        future.onFailure {error in
+            expectation.fulfill()
+        }
+        let mapped = future.map {value -> Try<Int> in
+            XCTAssert(false, "mapping called")
+            return Try(Int(1))
+        }
+        mapped.onSuccess {value in
+            XCTAssert(false, "mapped onSuccess called")
+        }
+        mapped.onFailure {error in
+            expectationMapped.fulfill()
+        }
+        future.failure(TestFailure.error)
+        waitForExpectationsWithTimeout(2) {error in
+            XCTAssertNil(error, "\(error)")
+        }
     }
 }
 
