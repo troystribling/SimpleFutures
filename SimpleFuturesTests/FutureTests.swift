dee6a73fa58dba8949704dd136d926d670cbaa6a
Troy Stribling <troy.stribling@gmail.com>
2014-12-18 09:08:46 -0800

    started flatmap tests

--- SimpleFuturesTests/FutureTests.swift
+++ SimpleFuturesTests/FutureTests.swift
@@ -517,3 +517,95 @@ class FutureMapTests : XCTestCase {
     }
 }
 
+class FutureFlatmapTests : XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testSuccessfulMapping() {
+        let future = Future<Bool>()
+        let expectationMapped = expectationWithDescription("OnSuccess fulfilled for mapped future")
+        let expectation = expectationWithDescription("OnSuccess fulfilled")
+        future.onSuccess {value in
+            XCTAssert(value, "future onSuccess value invalid")
+            expectation.fulfill()
+        }
+        future.onFailure {error in
+            XCTAssert(false, "future onFailure called")
+        }
+        let mapped = future.flatmap {value -> Future<Int> in
+            let promise = Promise<Int>()
+            promise.success(1)
+            return promise.future
+        }
+        mapped.onSuccess {value in
+            XCTAssertEqual(value, 1, "mapped onSuccess value invalid")
+            expectationMapped.fulfill()
+        }
+        mapped.onFailure {error in
+            XCTAssert(false, "mapped onFailure called")
+        }
+        future.success(true)
+        waitForExpectationsWithTimeout(2) {error in
+            XCTAssertNil(error, "\(error)")
+        }
+    }
+    
+//    func testFailedMapping() {
+//        let future = Future<Bool>()
+//        let expectationMapped = expectationWithDescription("OnFailure fulfilled for mapped future")
+//        let expectation = expectationWithDescription("OnSuccess fulfilled")
+//        future.onSuccess {value in
+//            XCTAssert(value, "future onSuccess value invalid")
+//            expectation.fulfill()
+//        }
+//        future.onFailure {error in
+//            XCTAssert(false, "future onFailure called")
+//        }
+//        let mapped = future.map {value -> Try<Int> in
+//            return Try<Int>(TestFailure.error)
+//        }
+//        mapped.onSuccess {value in
+//            XCTAssert(false, "mapped onSuccess called")
+//        }
+//        mapped.onFailure {error in
+//            expectationMapped.fulfill()
+//        }
+//        future.success(true)
+//        waitForExpectationsWithTimeout(2) {error in
+//            XCTAssertNil(error, "\(error)")
+//        }
+//    }
+//    
+//    func testMappingToFailedFuture() {
+//        let future = Future<Bool>()
+//        let expectationMapped = expectationWithDescription("OnFailure fulfilled for mapped future")
+//        let expectation = expectationWithDescription("OnFailure fulfilled")
+//        future.onSuccess {value in
+//            XCTAssert(false, "future onSucces called")
+//        }
+//        future.onFailure {error in
+//            expectation.fulfill()
+//        }
+//        let mapped = future.map {value -> Try<Int> in
+//            XCTAssert(false, "mapping called")
+//            return Try(Int(1))
+//        }
+//        mapped.onSuccess {value in
+//            XCTAssert(false, "mapped onSuccess called")
+//        }
+//        mapped.onFailure {error in
+//            expectationMapped.fulfill()
+//        }
+//        future.failure(TestFailure.error)
+//        waitForExpectationsWithTimeout(2) {error in
+//            XCTAssertNil(error, "\(error)")
+//        }
+//    }
+}
+
